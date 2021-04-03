5157f4866b02c81b8c10c5dfde73d667e6f30672
Marcio Fernandes <farciomernandes@gmail.com>
2021-04-03 12:57:02 -0300

    Add new text font and craeted search-box component.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.components';
 
 import './App.css';
 
@@ -8,12 +9,18 @@ import './App.css';
 class App extends Component {
   
   constructor(){
+    /**
+     * Super() extending the functionality that exist on component
+     * Methods and variables in the constructor are visible in this class. Includes componentDidMount
+     */
     super();
 
-    this.state = {
+    this.state = { //Similar use to useState hook
       monsters: [],
       searchField: ''
     }
+
+    
   }
 
   componentDidMount(){
@@ -23,18 +30,32 @@ class App extends Component {
     .then(users => this.setState({ monsters: users}))
   }
 
-  render(){
- return(
-      
-  <div className="App">
-    <input 
-      type="search" 
-      placeholder="search monsters" 
-      onChange={e=> this.setState({ searchField: e.target.value}, ()=> 
-      console.log(this.state))} />
+  handleChange = (e) =>{
+    this.setState({ searchField: e.target.value})
+    /**
+     * 1- This uses an arrow function for the compiler to automatically initialize bind () in
+     * the class by adding the function in context
+     * 2- So, if you don't use an arrow function, this function is not in the context of this class
+     */
+  }
 
-    <CardList monsters={this.state.monsters} />
-</div>
+  render(){
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+    )
+
+ return(
+    <div className="App">
+      <h1>Monsters Rolodex</h1>
+      <SearchBox
+        placeholder="search monsters"
+        handleChange={this.handleChange}
+      />
+     
+
+      <CardList monsters={filteredMonsters} />
+    </div>
  )
   }
 }
